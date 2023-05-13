73aa82d18974d8a92860da41be0e91b33327955d
reneta <renetaghristova@gmail.com>
2023-05-13 19:17:48 +0300

    add title-test

---
+++ title-test/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace title_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Инициализиране на Chrome driver
+            IWebDriver driver = new ChromeDriver();
+
+            // Навигиране до уеб страницата за тестване
+            driver.Navigate().GoToUrl("https://demoqa.com");
+
+            string title = driver.Title;
+
+            Console.WriteLine(title);
+
+            driver.Quit();
+        }
+    }
+}
